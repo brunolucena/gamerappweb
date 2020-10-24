f6f9c2b9c9ff1675189d485646293c490ac9ee22
Daniel <flaviodanielgs@gmail.com>
2020-10-24 19:04:25 -0300

    GamerRex

---
+++ src/Components/GamerRex/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import RexSrc from './rex.png';
+import { Rex } from './styles';
+
+const GamerRex: React.FC = () => {
+  return <Rex src={RexSrc} />;
+};
+
+export default GamerRex;
---
+++ src/Components/GamerRex/styles.ts
@@ -0,0 +1,2 @@
+import styled from 'styled-components';
+export const Rex = styled.img``;
