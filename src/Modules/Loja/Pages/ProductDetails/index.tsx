314f319036fdd85c32453f313db4ddc5ab020bfd
Bruno Lucena <bruno.lucenac@gmail.com>
2020-12-19 05:22:44 -0300

    ProductDetails. Finalizado.

--- src/Modules/Loja/Pages/ProductDetails/index.tsx
+++ src/Modules/Loja/Pages/ProductDetails/index.tsx
@@ -1,46 +1,140 @@
+import Box from 'Components/Box';
+import Button from 'Components/Button';
 import Container from 'Components/Container';
 import ContentSlider, { Item } from 'Components/ContentSlider';
+import Heading from 'Components/Heading';
 import React, { useEffect } from 'react';
+import Text from 'Components/Text';
+import { formatCurrency } from 'Helpers/formatters';
+import { loadProductDetails } from 'Modules/Loja/Store/Ducks/ProductDetails';
+import { ReduxStore } from 'Store/Redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import './styles.scss';
 
 const ProductDetails: React.FC = () => {
+  const dispatch = useDispatch();
   const { id } = useParams<{ id: string }>();
+  const { storeProductDetails } = useSelector((state: ReduxStore) => state);
 
-  const items: Array<Item> = [
-    {
-      id: '1',
-      url: 'https://olhardigital.com.br/wp-content/uploads/2020/09/20200925024455.jpg',
-    },
-    {
-      id: '2',
-      url: 'https://images8.alphacoders.com/710/thumb-1920-710329.jpg',
-    },
-    {
-      id: '3',
-      url: 'https://deathstrandingpc.505games.com/wp-content/themes/deathstranding/img/topimage.jpg',
-    },
-  ];
+  const { about, copyright, description, images, link, platforms, price, storeName, storeLogoUrl, title } = storeProductDetails;
+
+  const items: Item[] = images?.map((image) => ({ sessionId: '', imageUrl: image })) ?? [];
 
   useEffect(() => {
-    // TODO: get product details
-    // dispatch(getProductDetails({id}));
-
-    console.log({ id });
-  }, []);
+    dispatch(loadProductDetails({ id }));
+  }, [dispatch, id]);
 
   return (
-    <div className='product-details-container'>
+    <Box backgroundColor='#f8f8f8' className='product-details-container' minHeight='calc(100vh - 70px)'>
       <Container>
-        <div className='content'>
-          <div className='left'>
-            <ContentSlider items={items} itemsOnScreen={1} />
-          </div>
+        <Box display='grid' gap={30} gridTemplateColumns='2fr 1fr' paddingTop={60} paddingBottom={60}>
+          <Box display='flex' direction='column'>
+            <ContentSlider insideArrows items={items} itemsOnScreen={1} />
 
-          <div className='right'></div>
-        </div>
+            <Box direction='column' display='flex'>
+              <Box marginBottom={30} marginTop={30}>
+                <Heading weight='bold'>Sobre o jogo</Heading>
+              </Box>
+
+              <Text color='gray' size={18}>
+                {description}
+              </Text>
+            </Box>
+          </Box>
+
+          <Box>
+            <Box
+              backgroundColor='#ffffff'
+              borderTopRightRadius={10}
+              borderTopLeftRadius={10}
+              paddingTop={15}
+              paddingBottom={15}
+              paddingLeft={30}
+              paddingRight={30}
+            >
+              <Box marginBottom={12} marginTop={12}>
+                <Heading>{title}</Heading>
+              </Box>
+
+              <Text color='gray'>{about}</Text>
+
+              <Box alignItems='center' display='flex' gap={20} marginTop={24} marginBottom={30}>
+                <Box borderColor='green' borderStyle={1} paddingTop={8} paddingBottom={8} paddingLeft={8} paddingRight={8}>
+                  <Heading align='center' color='green' size={28}>
+                    -{price.discount}%
+                  </Heading>
+                </Box>
+
+                <Box display='flex' direction='column'>
+                  {price.oldPrice && (
+                    <Box left={2} position='relative'>
+                      <Text color='lightGray' lineThrough size={12}>
+                        {formatCurrency(price.oldPrice)}
+                      </Text>
+                    </Box>
+                  )}
+
+                  <Heading className='price'>{formatCurrency(price.price)}</Heading>
+                </Box>
+              </Box>
+
+              <Box marginTop={30} marginBottom={15}>
+                <Button external to={link}>
+                  <Heading align='center' className='button-text' color='white' size={20}>
+                    Comprar
+                  </Heading>
+                </Button>
+              </Box>
+            </Box>
+
+            <Box
+              alignItems='center'
+              backgroundColor='#f0f0f0'
+              borderBottomRightRadius={10}
+              borderBottomLeftRadius={10}
+              className='bottom'
+              display='flex'
+              justifyContent='between'
+              paddingTop={15}
+              paddingBottom={15}
+              paddingLeft={30}
+              paddingRight={30}
+            >
+              <Box display='flex' direction='column' gap={3}>
+                <Text size={14}>Ativação e sistemas</Text>
+
+                <Box>
+                  {platforms.map((platform, index) => {
+                    const { id, imageUrl, platformName } = platform;
+
+                    return (
+                      <Box key={id + index}>
+                        {imageUrl && <img src={imageUrl} alt={platformName} />}
+
+                        <Text weight='bold'>{platformName}</Text>
+                      </Box>
+                    );
+                  })}
+                </Box>
+              </Box>
+
+              <Box alignItems='end' className='right' display='flex' direction='column' gap={3}>
+                <Text size={14}>Vendido por</Text>
+
+                <Box>{storeLogoUrl ? <img src={storeLogoUrl} alt={storeName} /> : <Text weight='bold'>{storeName}</Text>}</Box>
+              </Box>
+            </Box>
+          </Box>
+        </Box>
+
+        {copyright && (
+          <Box marginTop={40}>
+            <Text color='lightGray'>{copyright}</Text>
+          </Box>
+        )}
       </Container>
-    </div>
+    </Box>
   );
 };
 
