cd685e718450359f248a457cdc0461f21e5ded01
Bruno Lucena <bruno.lucenac@gmail.com>
2020-12-19 05:07:42 -0300

    Add property external.

--- src/Components/Button/index.tsx
+++ src/Components/Button/index.tsx
@@ -6,17 +6,20 @@ type ButtonType = 'primary' | 'secondary';
 
 interface Props {
   buttonType?: 'button' | 'submit' | 'reset';
+  external?: boolean;
   onClick?: ((event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void) | undefined;
   to?: string;
   type?: string;
 }
 
-const Button: React.FC<Props> = (props) => {
-  const { buttonType, children, onClick, to, type } = props;
-
+const Button: React.FC<Props> = ({ buttonType, children, external, onClick, to, type }) => {
   const button = <BtnStyles type2={type || ''}>{children}</BtnStyles>;
 
-  return to ? (
+  return external ? (
+    <a href={to} rel='noopened noreferrer' style={{ textAlign: 'center', textDecoration: 'none' }} target='_blank'>
+      {button}
+    </a>
+  ) : to ? (
     <Link to={to} style={{ textAlign: 'center', textDecoration: 'none' }}>
       {button}
     </Link>
