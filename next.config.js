9de091799a75dab2cdc100618d0f87852cb2d44b
Bruno Lucena <bruno.lucenac@gmail.com>
2021-01-14 20:34:29 -0300

    CardStore. Ajustar imagens para carregar como imagens e não background.

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  images: {
+    domains: [
+      'assets.nuuvem.com',
+      'images.igdb.com',
+    ],
+  },
+};
--- src/components/CardStore/index.tsx
+++ src/components/CardStore/index.tsx
@@ -2,15 +2,14 @@ import Alarm from '@material-ui/icons/Alarm';
 import Box from 'components/Box';
 import Countdown from 'react-countdown';
 import IconPlatform from 'components/IconsPlatform';
+import Image from 'next/image';
 import Link from 'next/link';
 import moment from 'moment';
 import styles from './styles.module.scss';
 import Text from 'components/Text';
 import { formatCurrency } from 'helpers/formatters';
-import { getProportions } from 'helpers/functions';
 import { Grid } from '@material-ui/core';
 import { ProductPlatform } from 'modules/Loja/Lib/ProductDetails/models';
-import { SizeMe } from 'react-sizeme';
 
 interface Props {
   banner: string;
@@ -66,82 +65,64 @@ function CardStore({
   return (
     <Link href={`/produto/${id}`}>
       <a className={styles.containerCardStore}>
-        <SizeMe>
-          {({ size }) => {
-            const proportions = getProportions({ width: size.width ?? 200 }, '2x1');
+        <div className={styles.banner}>
+          <Image alt={title} src={banner} layout="fill" objectFit="cover" />
 
-            return (
-              <>
-                <div
-                  className={styles.banner}
-                  style={{
-                    backgroundImage: `url(${banner})`,
-                    backgroundPosition: 'center',
-                    backgroundRepeat: 'none',
-                    backgroundSize: 'cover',
-                    borderRadius: '8px 8px 0px 0px',
-                    width: '100%',
-                    height: proportions.height,
-                  }}
-                >
-                  {duration && moment(duration).isAfter(dateHourNow) && (
-                    <Box className={styles.containerCountDown}>
-                      {countdown}
-                    </Box>
-                  )}
+          {duration && moment(duration).isAfter(dateHourNow) && (
+            <Box className={styles.containerCountDown}>
+              {countdown}
+            </Box>
+          )}
+        </div>
+
+        <div className={styles.content}>
+          <div className={styles.body}>
+            <Grid container spacing={1}>
+              <Grid item xs={12} md={12}>
+                <p className={styles.title}>{title}</p>
+              </Grid>
+            </Grid>
+
+            <Grid container spacing={1} alignItems='center'>
+              {(moment(duration).isAfter(dateHourNow) && discount && discount > 0) && (
+                <div className={styles['discount-wrapper']}>
+                  <span className={`${styles.badge} ${styles['--primary']}`}>-{discount}%</span>
                 </div>
-                <div className={styles.content}>
-                  <div className={styles.body}>
-                    <Grid container spacing={1}>
-                      <Grid item xs={12} md={12}>
-                        <p className={styles.title}>{title}</p>
-                      </Grid>
-                    </Grid>
+              )}
 
-                    <Grid container spacing={1} alignItems='center'>
-                      {(moment(duration).isAfter(dateHourNow) && discount && discount > 0) && (
-                        <div className={styles['discount-wrapper']}>
-                          <span className={`${styles.badge} ${styles['--primary']}`}>-{discount}%</span>
-                        </div>
-                      )}
-
-                      <Grid item xs={discount && discount > 0 ? 6 : 12} md={discount && discount > 0 ? 6 : 12}>
-                        {isAvailable ? (
-                          <Text className={styles.price} weight='bold'>
-                            {value
-                              ? <>
-                                {oldPrice ? (
-                                  <Text color='lightGray' lineThrough size={12}>
-                                    {formatCurrency(oldPrice)}
-                                  </Text>
-                                ) : (
-                                  ''
-                                )}
-                                {formatCurrency(value)}
-                              </>
-                              : <span>Gratuito</span>
-                            }
+              <Grid item xs={discount && discount > 0 ? 6 : 12} md={discount && discount > 0 ? 6 : 12}>
+                {isAvailable ? (
+                  <Text className={styles.price} weight='bold'>
+                    {value
+                      ? <>
+                        {oldPrice ? (
+                          <Text color='lightGray' lineThrough size={12}>
+                            {formatCurrency(oldPrice)}
                           </Text>
                         ) : (
-                            <Text weight='bold'>Indisponível</Text>
+                          ''
                         )}
-                      </Grid>
-                    </Grid>
-                  </div>
-                  <div className={styles.footer}>
-                    {platforms?.map((platform, index) => (
-                      <IconPlatform
-                        key={platform.id + index}
-                        name={platform.platformName}
-                        icon={platform.imageUrl}
-                      />
-                    ))}
-                  </div>
-                </div>
-              </>
-            );
-          }}
-        </SizeMe>
+                        {formatCurrency(value)}
+                      </>
+                      : <span>Gratuito</span>
+                    }
+                  </Text>
+                ) : (
+                    <Text weight='bold'>Indisponível</Text>
+                )}
+              </Grid>
+            </Grid>
+          </div>
+          <div className={styles.footer}>
+            {platforms?.map((platform, index) => (
+              <IconPlatform
+                key={platform.id + index}
+                name={platform.platformName}
+                icon={platform.imageUrl}
+              />
+            ))}
+          </div>
+        </div>
       </a>
     </Link>
   );
